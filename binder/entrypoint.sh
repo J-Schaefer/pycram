60105df9ebc422cf5b73baa6013b27c555b36ea0
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2024-09-06 14:47:02 +0200

    Edit binder setup

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -2,6 +2,9 @@
 
 source ${PYCRAM_WS}/devel/setup.bash
 roscore &
+roslaunch --wait roslaunch pycram ik_and_description.launch &
+roslaunch --wait iai_kitchen upload_kitchen_obj.launch &
+roslaunch --wait dagappy dagap.launch &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
 cp ${PYCRAM_WS}/src/pycram/binder/webapps.json ${PYCRAM_WS}/src/rvizweb/webapps/app.json 
 
