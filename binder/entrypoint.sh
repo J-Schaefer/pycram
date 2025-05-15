c81048a43f450fd666aca55bc6bb885e04c3e884
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2025-05-15 16:03:49 +0200

    Fix binder and Docker scripts
    Fix wrong commands and add missing
    dependencies to the install files.

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -2,7 +2,7 @@
 
 source ${PYCRAM_WS}/devel/setup.bash
 roscore &
-roslaunch --wait roslaunch pycram ik_and_description.launch &
+roslaunch --wait pycram ik_and_description.launch &
 roslaunch --wait iai_kitchen upload_kitchen_obj.launch &
 roslaunch --wait dagappy dagap.launch &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${PYCRAM_WS}/src/pycram/binder/rviz_configs/pr2_config.json &
